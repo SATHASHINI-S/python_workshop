8a2b0208d74a8ea1ec13d957f2685d190cde201b
Sathashini <sathashini4623@gmail.com>
2025-08-22 11:37:56 +0530

    Flask

---
+++ basic/noGenerator.py
@@ -0,0 +1,17 @@
+# import random 
+
+# number = number.randint(1,10)
+# guess = 0
+
+# while guess != number:
+
+
+a=10
+b=20
+c=14
+if(a>b and a>c):
+    print("A is greatest")
+elif(b>c and b>a):
+    print("B is greatest")
+else:
+    print("C is greatest")
